981c4c87853f7d3eba6c263b1c66d3a26d9dcac3
angel.rios <angel.rios.dev@gmail.com>
2026-02-03 09:40:10 -0600

    3 level's of structure tf projects

--- modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  value       = aws_instance.this.id
-  description = "The ID of the EC2 instance"
-}
-
-output "public_ip" {
-  value       = aws_instance.this.public_ip
-  description = "Public IP address of the EC2 instance"
-}
