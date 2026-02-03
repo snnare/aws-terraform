c99144011775112b0e065e9086fa8914c527fac5
angel.rios <angel.rios.dev@gmail.com>
2026-02-03 09:53:36 -0600

    Add variables.tf for separate and module the main.tf

---
+++ v1/dev/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+ cidr_block = var.cidr_block
+
+enable_dns_support = false
+ enable_dns_hostnames = false
+ tags = { 
+    Name = "${var.env}-main"
+ }
+}
